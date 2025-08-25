f51614ea7dc3b33131759b3fc9e4d74c70bada83
sallytesip-jpg <sallytesip@gmail.com>
2025-08-25 02:19:45 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'cultivo-pro-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
