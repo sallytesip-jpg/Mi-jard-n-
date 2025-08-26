bf85681dbb0e106914376d1aeb67422c5e8d802b
sallytesip-jpg <sallytesip@gmail.com>
2025-08-26 12:25:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,39 @@
-// sw.js
 const CACHE_NAME = "cultivo-pro-v1";
-const urlsToCache = [
-  "/",              // la raíz
-  "/index.html",    // tu index
-  "/manifest.json", // el manifest
-  "/icons/icon-192.png", // iconos
+const URLS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
   "/icons/icon-512.png"
 ];
 
-// Instalar y cachear archivos
+// Instalar y guardar en caché
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+      return cache.addAll(URLS_TO_CACHE);
     })
+  );
+});
+
+// Activar y limpiar cachés viejos
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      )
+    )
   );
 });
 
 // Servir desde caché
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((resp) => {
-      return resp || fetch(event.request);
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
-  );
-});
-
-// Actualizar SW
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
-    )
   );
 });
